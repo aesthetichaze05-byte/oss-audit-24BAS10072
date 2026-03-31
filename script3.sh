8b46e5e36a72fdc0dd42f43bac9b0095e14c0058
aesthetichaze05-byte <aesthetichaze05@gmail.com>
2026-03-31 21:18:09 +0530

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# ==========================================
+# Script 3: Disk and Permission Auditor
+# Author: [Your Name]
+# Description:
+# Checks important Linux directories
+# Shows permissions and disk usage
+# ==========================================
+
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
+
+echo "Directory Audit Report"
+echo "-----------------------------------"
+
+for DIR in "${DIRS[@]}"
+do
+
+ if [ -d "$DIR" ]
+ then
+
+ PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+
+ SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+
+ echo "$DIR"
+ echo "Permissions : $PERMS"
+ echo "Size        : $SIZE"
+
+ echo "-----------------------------------"
+
+ else
+
+ echo "$DIR not found"
+
+ fi
+
+done
